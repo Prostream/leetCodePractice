a9bdfa0a1c3de3ebd3d3280c5eb939ab0e883311
jimyh <jimyhacker@outlook.com>
2025-10-17 18:33:42 -0700

    10/17 模板：Tries

---
+++ template/tries.py
@@ -0,0 +1,33 @@
+#字典树 tries 用于字符串记录，要能insert，要能search 要能startsWith
+class TrieNode(object):
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Tries(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end
+
+    def startsWith(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
